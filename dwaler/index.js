4435ded8d30387576d269f03452c6feff581a8b0
Koen Schmeets <k.schmeets@gmail.com>
2016-07-09 13:32:15 +0200

    Trigger build

--- dwaler/index.js
+++ dwaler/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', (req, res) =>
-  res.send('<h1>Dwaler !!!</h1>'))
+  res.send('<h1>Dwaler =)</h1>'))
 
 app.listen(process.env.DWALER_PORT, () => 
   console.log(`Example app listening on port ${process.env.DWALER_PORT}!`))
