fd3a8a54f2da702916da3c09bc1ea35223eca840
Koen Schmeets <k.schmeets@gmail.com>
2016-07-09 09:54:15 +0200

    Updating script

--- sftp.sh
+++ sftp.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-for user_home in /home/* ; do
-  if [ -d "$user_home" ]; then
-    username=`basename $user_home`
-    echo "Setup $user_home/share folder for $username"
-    mkdir -p $user_home/share/vespakoen/ontheweb
-    chown -R $username:users $user_home/share
-  fi
-done
+
+echo "Creating and chowning cache folder"
+chown -R cache:users /home/cache/share
