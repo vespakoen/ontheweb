9c8b696e8223b83dcd10e40a08c74e09b6d39fb0
Koen Schmeets <k.schmeets@gmail.com>
2016-07-09 13:30:10 +0200

    Updating configs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 . ./env.sh
 docker-machine ssh ks mkdir /config
+docker-machine scp ./nginx.conf.erb ks:/config/
 docker-machine scp ./default.conf.erb ks:/config/
 docker-machine scp ./default.ssl.conf.erb ks:/config/
 docker-machine scp ./sftp.sh ks:/config/
 docker-compose pull
-docker-compose up -d
+docker-compose up -d --remove-orphans
 docker-compose logs -f
