151d09cae2d0f1c11ccd02f6c5ede2f2c3b3ef40
Koen Schmeets <k.schmeets@gmail.com>
2016-07-09 09:04:03 +0200

    Pull new images, use latest drone/drone image

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 docker-machine ssh ks mkdir /config
 docker-machine scp ./default.conf.erb ks:/config/
 docker-machine scp ./default.ssl.conf.erb ks:/config/
+docker-compose pull
 docker-compose up -d
 docker-compose logs -f
