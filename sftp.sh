fde53189a879930064a6b45264d38f3e5baa247b
Koen Schmeets <k.schmeets@gmail.com>
2016-07-09 09:46:17 +0200

    Updating sftp.sh script to create folder

--- sftp.sh
+++ sftp.sh
@@ -3,7 +3,7 @@ for user_home in /home/* ; do
   if [ -d "$user_home" ]; then
     username=`basename $user_home`
     echo "Setup $user_home/share folder for $username"
-    mkdir -p $user_home/share
+    mkdir -p $user_home/share/vespakoen/ontheweb
     chown -R $username:users $user_home/share
   fi
 done
