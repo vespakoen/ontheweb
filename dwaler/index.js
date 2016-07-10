b5ea89e03df705db8871c9760e6a6c4c4598121c
Koen Schmeets <k.schmeets@gmail.com>
2016-07-11 00:11:40 +0200

    [dwaler] Add public folder, serve it

--- dwaler/index.js
+++ dwaler/index.js
@@ -1,8 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.get('/', (req, res) =>
-  res.send('<h1>Welcome to Dwaler</h1>'))
+app.use(express.static('public'));
 
 app.listen(process.env.DWALER_PORT, () => 
   console.log(`Example app listening on port ${process.env.DWALER_PORT}!`))
