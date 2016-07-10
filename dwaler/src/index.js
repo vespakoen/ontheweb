372d76d84bc656bbb2f05fef001f6176ebdbd98e
Koen Schmeets <k.schmeets@gmail.com>
2016-07-11 01:07:59 +0200

    [dwaler] Add some react

--- dwaler/src/index.js
+++ dwaler/src/index.js
@@ -1,8 +1,45 @@
 import 'whatwg-fetch'
 import config from '../config'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
-fetch(config.TOP_TRACKS_URL)
-  .then(res => res.json())
-  .then(result => {
-    console.log(result)
-  })
+class App extends React.Component {
+  componentDidMount() {
+    fetch(config.TOP_TRACKS_URL)
+      .then(res => res.json())
+      .then(tracks => this.setState({ tracks }))
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <AppBar
+          title="Dwaler"
+          iconElementRight={
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="Help" />
+              <MenuItem primaryText="Sign out" />
+            </IconMenu>
+          }
+        />
+        <h1>Dwaler</h1>
+        Welcome dwaler
+        { JSON.stringify(this.state.tracks) }
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
