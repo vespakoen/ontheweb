f108a9518553b8430caad685208c8d74e3035cc8
Koen Schmeets <k.schmeets@gmail.com>
2016-07-27 05:08:12 +0200

    Use custom images

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 . ./env.sh
-docker-machine ssh ks mkdir /config 2> /dev/null
-docker-machine scp ./nginx.conf.erb ks:/config/
-docker-machine scp ./default.conf.erb ks:/config/
-docker-machine scp ./default.ssl.conf.erb ks:/config/
-docker-machine scp ./sftp.sh ks:/config/
 docker-compose pull
+docker-compose stop -f
 docker-compose rm --all
 docker-compose up -d --remove-orphans
 docker-compose logs -f
