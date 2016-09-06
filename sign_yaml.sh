b809820d36e20c0e395f51cb268061a28cea673d
Koen Schmeets <k.schmeets@gmail.com>
2016-09-06 14:45:11 +0200

    Adding overpass api

--- sign_yaml.sh
+++ sign_yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 . ./env.sh
-eval "$(docker-machine env --unset)"
+eval "$(docker-machine env default)"
 echo "Signing .drone.yml"
 docker run -v $(pwd):/project --rm drone/drone:0.5 -t $DRONE_USER_TOKEN -s http://176.9.59.53:8000 sign vespakoen/ontheweb --in "/project/.drone.yml" --out "/project/.drone.yml.sig"
