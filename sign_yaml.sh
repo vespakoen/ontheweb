0b87b7b8676f20f5ed4b4c4372fe8ce1cc280254
Koen Schmeets <k.schmeets@gmail.com>
2016-09-18 01:33:20 +0200

    Updating package description

--- sign_yaml.sh
+++ sign_yaml.sh
@@ -3,4 +3,4 @@
 . ./env.sh
 eval "$(docker-machine env default)"
 echo "Signing .drone.yml"
-docker run -v $(pwd):/project --rm drone/drone:0.5 -t $DRONE_USER_TOKEN -s http://176.9.59.53:8000 sign vespakoen/ontheweb --in "/project/.drone.yml" --out "/project/.drone.yml.sig"
+docker run -v $(pwd):/project --rm drone/drone:0.5 -t $DRONE_USER_TOKEN -s https://ci.koenschmeets.nl sign vespakoen/ontheweb --in "/project/.drone.yml" --out "/project/.drone.yml.sig"
