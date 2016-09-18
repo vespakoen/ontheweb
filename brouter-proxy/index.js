f87c134883839861821a9475ac6646a81c21521f
Koen Schmeets <k.schmeets@gmail.com>
2016-09-18 03:07:52 +0200

    Fixed =D

--- brouter-proxy/index.js
+++ brouter-proxy/index.js
@@ -2,9 +2,20 @@ const http = require('http');
 const spawn = require('child_process').spawn;
 
 const proxy = http.createServer((req, res) => {
-  var req = spawn('curl', ['http://h2096617.stratoserver.net:443/brouter/?' + req.url.split('?')[1]])
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  req.stdout.pipe(res)
+  var body = ''
+  const preq = spawn('curl', ['http://h2096617.stratoserver.net:443/brouter/?' + req.url.split('?')[1]])
+  preq.stdout.setEncoding('utf8')
+  preq.stdout.on('data', chunk => {
+  	body += chunk
+  })
+  preq.stdout.on('end', () => {
+  	const buff = new Buffer(body, 'utf8')
+  	res.writeHead(200, {
+  		'Content-Lenth': buff.length,
+  		'Content-Type': 'application/json'
+  	})
+  	res.end(buff)
+  })
 });
 
 proxy.listen(process.env.BROUTER_PROXY_PORT, () => {
