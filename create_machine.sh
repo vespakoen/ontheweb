6586316411cffd50bb080f871cb9514863440b60
Koen Schmeets <k.schmeets@gmail.com>
2017-03-09 03:04:20 +0100

    Updates

--- create_machine.sh
+++ create_machine.sh
@@ -2,21 +2,21 @@
 
 . ./env.sh
 
-# docker-machine create \
-#   --driver digitalocean \
-#   --digitalocean-access-token=$DIGITALOCEAN_ACCESS_TOKEN \
-#   --digitalocean-region=fra1 \
-#   --digitalocean-image "ubuntu-16-04-x64" \
-#   --digitalocean-size=1gb \
-#   --digitalocean-ipv6 \
-#   $MACHINE_NAME
-
-# docker-machine scp ./add_swap.sh $MACHINE_NAME:/add_swap.sh
-# docker-machine ssh $MACHINE_NAME chmod +x /add_swap.sh
-# docker-machine ssh $MACHINE_NAME sudo /add_swap.sh
-
-docker-machine --debug create \
-  --driver generic \
-  --generic-ip-address 148.251.12.99 \
-  --generic-ssh-key ~/.ssh/id_rsa \
+docker-machine create \
+  --driver digitalocean \
+  --digitalocean-access-token=$DIGITALOCEAN_ACCESS_TOKEN \
+  --digitalocean-region=fra1 \
+  --digitalocean-image "ubuntu-16-04-x64" \
+  --digitalocean-size=1gb \
+  --digitalocean-ipv6 \
   $MACHINE_NAME
+
+docker-machine scp ./add_swap.sh $MACHINE_NAME:/add_swap.sh
+docker-machine ssh $MACHINE_NAME chmod +x /add_swap.sh
+docker-machine ssh $MACHINE_NAME sudo /add_swap.sh
+
+# docker-machine --debug create \
+#   --driver generic \
+#   --generic-ip-address 148.251.12.99 \
+#   --generic-ssh-key ~/.ssh/id_rsa \
+#   $MACHINE_NAME
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 . ./env.sh
-# docker-compose pull
+docker-compose pull
 docker-compose stop
-# docker-compose rm
+docker-compose rm
 docker-compose up -d --remove-orphans
 docker-compose logs -f
